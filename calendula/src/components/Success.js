f2aafd38088bf037f0bd4d108c9fb89a50c573fa
Dylan Nguyen <dylan.nguyen@duke.edu>
2023-09-09 18:56:16 -0400

    success for latham to work on

---
+++ calendula/src/components/Success.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './Success.css';
+
+function Success() {
+  return (
+    <div>
+      <p>hi</p>
+    </div>
+  );
+}
+
+export default Success;
--- calendula/src/index.js
+++ calendula/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Success from './components/Success'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Success />
   </React.StrictMode>
 );
 
